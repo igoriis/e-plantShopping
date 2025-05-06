56f9f26e379cd4e13da94fead0e5f2bb8dfcf879
igoriis <igor.iis.dev@gmail.com>
2025-05-06 10:43:15 -0400

    in progress..

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addItem, removeItem, updateQuantity } from "./CartSlice";
 import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
+    const cart = useSelector(state => state.cart.items);
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -232,7 +236,9 @@ function ProductList({ onHomeClick }) {
         fontSize: '30px',
         textDecoration: 'none',
     }
-
+    const styleH1Div = {
+        textAlign: 'center',
+    }
     const handleHomeClick = (e) => {
         e.preventDefault();
         onHomeClick();
@@ -252,6 +258,23 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product)); 
+        setAddedToCart((prevState) => ({
+            ...prevState, 
+            [product.name]: true, 
+        }));
+    };
+
+    const calculateCount = () => {
+        let total = 0;
+        cart.forEach((item) => {
+            total += item.quantity;
+        });
+        return total;
+      };
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -269,12 +292,42 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'>{calculateCount()} <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
             {!showCart ? (
                 <div className="product-grid">
-
+                    {plantsArray.map((category, index) => ( 
+                    <div key={index}> 
+                        <h1>
+                        <div style={styleH1Div}>{category.category}</div> 
+                        </h1>
+                        <div className="product-list">
+                        {category.plants.map((plant, plantIndex) => {
+                            const isAlreadyInCart = cart.some(item => item.name === plant.name);
+                            return ( 
+                            <div className="product-card" key={plantIndex}> 
+                            <img 
+                                className="product-image" 
+                                src={plant.image} 
+                                alt={plant.name} 
+                            />
+                            <div className="product-title">{plant.name}</div>
+                            
+                            <div className="product-description">{plant.description}</div> 
+                            <div className="product-cost">{plant.cost}</div> 
+                            <button
+                                className={`product-button ${isAlreadyInCart ? 'disabled' : ''}`}
+                                disabled={isAlreadyInCart}
+                                onClick={() => handleAddToCart(plant)} 
+                            >
+                                {isAlreadyInCart ? 'Added' : 'Add to Cart'}
+                            </button>
+                            </div>
+                        )})}
+                        </div>
+                    </div>
+                    ))}
 
                 </div>
             ) : (
